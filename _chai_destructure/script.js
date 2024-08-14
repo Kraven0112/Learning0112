d343f24b27470504c12ca4b9c2c94c62b3d5fd2b
Sushil_The_Coder0112 <167796699+Kraven0112@users.noreply.github.com>
2024-08-14 19:57:23 +0000

    change file

--- _chai_destructure/script.js
+++ _chai_destructure/script.js
@@ -1,1 +1,34 @@
-console.log("Hello Sushil Chaudhary")
+'use strict'
+console.log("Coding Start")
+console.log("--------------------------")
+
+/*
+Destructre in javascript
+ */
+const user={
+    firstName:"Sushil",
+    lastName:"Chaudhary",
+    age:21,
+    country:"Nepal"
+}
+
+
+const{firstName:username,lastName:usersurname,age:userage,country:usercountry} = user
+// console.table([username,usersurname,userage,usercountry])
+
+
+const  strNum = null
+// console.log(typeof(strNum))
+// console.log(typeof strNum)
+
+let value = Number(strNum)
+// console.log(value)
+// console.log(typeof value)
+// console.log(typeof(value))
+
+
+const isBoolean = " "
+console.log(Boolean(isBoolean))
+
+console.log("--------------------------")
+console.log("Coding end") 
