0149a1dc512fe430f4659c33a91cb369deef629e
Sushil_The_Coder0112 <167796699+Kraven0112@users.noreply.github.com>
2024-08-15 20:46:30 +0000

    add new file

--- _chai_function/function.js
+++ _chai_function/function.js
@@ -7,9 +7,23 @@ const number = Number(score1)
 
 let anything = 0
 let value = Boolean(anything)
-console.log(value)
+// console.log(value)
 
 // 1 => true  0=>false
 // "" => false
 // " " => true
 // "hello" => true
+
+// console.log("2" == 2 ) //equality check --conversion happen
+// console.log("2" === 2 ) //type and equality check
+
+
+// Is javascript dynamically typed or statically typed language ? 
+
+// const id1 = Symbol('123')
+// const id2 = Symbol('123')
+// console.log(id1)
+// console.log(id2)
+
+const bigInt = 1234562345653892n
+console.log(typeof bigInt)
