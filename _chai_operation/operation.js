0b728fc4856baf6890efb375c455bc530d380207
Sushil_The_Coder0112 <167796699+Kraven0112@users.noreply.github.com>
2024-08-15 07:28:38 +0000

    new file

---
+++ _chai_operation/operation.js
@@ -0,0 +1,16 @@
+// operation
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(+true)
+// console.log(+false)
+
+// post-increment and prefix increment
+let num1 = 121
+++num1
+console.log(num1)
+
+let num2 = 22
+num2++
+console.log(num2)
+
