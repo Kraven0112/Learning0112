f1f29915873431cabeca3430a6cf77256778033a
Sushil_The_Coder0112 <167796699+Kraven0112@users.noreply.github.com>
2024-08-15 20:01:25 +0000

    oho

---
+++ _chai_function/function.js
@@ -0,0 +1,15 @@
+const score = 33
+const score1 = false
+const number = Number(score1)
+
+// console.log(number)
+// console.log(parseInt(score1))
+
+let anything = 0
+let value = Boolean(anything)
+console.log(value)
+
+// 1 => true  0=>false
+// "" => false
+// " " => true
+// "hello" => true
